8ea7e91cd9f8e8a2aca7df8b0bdd6cd9bb302951
Andrea R <amrdev08@gmail.com>
2018-02-11 11:59:48 -0500

    Showing message if server is offline

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,12 @@
-self.addEventListener("fetch", function(event){
-	event.respondWith(
-		new Response("Hello World"))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).then(res => {
+      if(res.status === 404) {
+        return new Response("Not found")
+      }
+      return res;
+    }).catch(err => {
+      return new Response("page offline")
+    })
+  );
 });
