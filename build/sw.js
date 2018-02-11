978755a281d4feeaf8cf6fa967cf1752dd2ec1f2
Andrea R <amrdev08@gmail.com>
2018-02-11 12:46:44 -0500

    sw caching images. No more builds can only update in 8080 from now on

---
+++ build/sw.js
@@ -0,0 +1,29 @@
+self.addEventListener("install", event => {
+  var urlsToCache = [
+    "/",
+    "/static/js/main.84e64a11.js",
+    "/static/css/main.65027555.css",
+    "/index.html"
+  ]
+
+  event.waitUntil(
+    caches.open("nyt-static-v4").then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  )
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.delete("nyt-static-v3")
+  );
+})
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      if(res) return res;
+      return fetch(event.request);
+    })
+  );
+});
