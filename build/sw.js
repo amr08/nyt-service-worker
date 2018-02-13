3f5642cf67985379087010b3e598ead0d2426685
Andrea R <amrdev08@gmail.com>
2018-02-12 20:09:41 -0500

    deploying from branch

---
+++ build/sw.js
@@ -0,0 +1,30 @@
+self.addEventListener("install", event => {
+  //Cool!!!!!!
+  var urlsToCache = [
+    "/",
+    "/static/js/main.24567533.js",
+    "/static/css/main.65027555.css",
+    "/index.html" 
+  ]
+
+  event.waitUntil(
+    caches.open("nyt-static-v4").then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  )
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.delete("nyt-static-v3")
+  );
+})
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => {
+      if(res) return res;
+      return fetch(event.request);
+    })
+  );
+});
