3ba02ecbcebd14a58d23cd26501c98faaddd8c1b
Andrea R <amrdev08@gmail.com>
2018-02-18 20:26:10 -0500

    hopefully last time

--- build/sw.js
+++ build/sw.js
@@ -1,5 +1,5 @@
 self.addEventListener("install", event => {
-  //Cool!!!!!
+  //Cool!!!!!!!
   var urlsToCache = [
     "/",
     "/static/js/main.8db09a36.js",
@@ -8,7 +8,7 @@ self.addEventListener("install", event => {
   ]
 
   event.waitUntil(
-    caches.open("nyt-static-v10").then(cache => {
+    caches.open("nyt-static-v11").then(cache => {
       return cache.addAll(urlsToCache);
     })
   )
@@ -16,7 +16,7 @@ self.addEventListener("install", event => {
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.delete("nyt-static-v9")
+    caches.delete("nyt-static-v10")
   );
 })
 
